ed8dc20ed76950d2f85a3af6eab8bf37ba043f10
Hamed-Ajaj <hamedsgaming@gmail.com>
2024-01-29 20:34:20 +0200

    files updated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,9 @@
-import { useState,useEffect } from "react"
-import { Box,Stack,Typography } from "@mui/material"
-import { Sidebar,Videos } from "./"
-import {fetchFromApi} from "../utils/fetchFromApi"
+import { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchFromApi } from "../utils/fetchFromApi"; 
+import { Videos, Sidebar } from "./";
+
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
   const [videos, setVideos] = useState(null);
@@ -14,16 +16,16 @@ const Feed = () => {
     }, [selectedCategory]);
 
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row",overflowY:"column" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 0.8 } }}>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 JSM Media
+          Copyright © 2024 Hamed Ajaj
         </Typography>
       </Box>
 
-      <Box p={1.6} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 ,justifyContent:"center"}}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
           {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
@@ -31,7 +33,7 @@ const Feed = () => {
         <Videos videos={videos} />
       </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
